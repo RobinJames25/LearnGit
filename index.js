1a45d4c053fd82624c1f508ad2baa36c9d76e124
RobinJames25 <jamesrobin12432@gmail>
2024-03-11 12:52:47 +0300

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello wrld!";)
+/*let a=5;
+
+console.log(a)
+*/
+//let myName="Robin";
+/*myName="Obiri";
+myName=5;
+*/
+console.log(myName);
+
+let myNumber=5;
+//myNumber===5;
+
+if(myNumber===5){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
